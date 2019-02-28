e043c82d38a705e37dab1531ff6554f942acd532
PedroPinheiro62 <pedrojaironpn@gmail.com>
2019-02-28 19:58:06 -0300

    Correcao do codigo main.c fornecido para que o programa cumpra a funcao desejada.

--- src/main.c
+++ src/main.c
@@ -5,18 +5,53 @@
  * numero de palavras separadas que recebeu e, apos, encerrar.
  */
 
+/*Aluno: Pedro Jairo Nogueira Pinheiro Neto
+ *RA: 156992
+ */
+
 #include <stdio.h>
 
 int main() {
+	//Contador inicializado em 0
+	int counter;
+  	char c;
+	
+	counter = 0;
+	
+	//Percorremos a serie de caracteres um a um checando seus valores com a tabela ASCII.
+  	do{
+		//Se encontramos uma letra, trata-se de uma palavra.
+		if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)){
+			counter = counter + 1;
+			//Percorremos a palavra ate o fim e adicionamos 1 ao contador.
+			while ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)){
+				scanf("%c", &c);
+			}
+		}
+		//Se encontramos um algorismo, trata-se de um numero.
+		else if(c >= 48 && c <= 57){
+			counter = counter + 1;
+			//Percorremos um numero ate o fim.
+			while (c >= 48 && c <= 57){
+				scanf("%c", &c);
+			}
+			//checamos se o numero eh um numero decimal separado por um ponto.
+			if (c = 46){
+				scanf("%c", &c);
+				if(c >= 48 && c <= 57){
+					while (c >= 48 && c <= 57){
+						scanf("%c", &c);
+					}
+				}
+			}
+		}
+		//Se nao se trata nem de um algorismo nem de uma letra, pegamos o proximo caractere da serie.
+		else{
+			scanf("%c", &c);
+		}
+  	}while (c != '\n'); 
 
-  char c;
-
-  c = 1;
-
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
-
-  printf("1\n");
-  return 0;
+	//Impressao do numero de palavras presentes na serie de caracteres.
+  	printf("%d\n", counter);
+  	return 0;
 }
